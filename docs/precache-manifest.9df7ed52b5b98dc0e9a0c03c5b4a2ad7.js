72ee9244575b3683042e34b391cf8a3ee8d3c163
Roman Maksimov <parker2003@mail.ru>
2020-09-23 18:31:08 +0300

    Compiled bundle

---
+++ docs/precache-manifest.9df7ed52b5b98dc0e9a0c03c5b4a2ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe4f892fe1bfba1cdc9890d008cad54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e96afb5e429571aeb0a",
+    "url": "/static/css/main.9e7543f9.chunk.css"
+  },
+  {
+    "revision": "fa6f722ae037938f2793",
+    "url": "/static/js/2.798e6481.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.798e6481.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e96afb5e429571aeb0a",
+    "url": "/static/js/main.55d59d73.chunk.js"
+  },
+  {
+    "revision": "935c5bb58badd06d505a",
+    "url": "/static/js/runtime-main.e83fdd84.js"
+  }
+]);
